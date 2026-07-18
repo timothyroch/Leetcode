5924d5250b1b77abfbcc814b74ac847b95e38d52
Timothy Roch <timothyroch@gmail.com>
2026-07-18 17:44:47 -0400

    Implement isAnagram method in Solution class

---
+++ HashMap/242. Valid Anagram/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] counts = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            counts[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            int index = t.charAt(i) - 'a';
+
+            if (--counts[index] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
